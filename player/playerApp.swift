2dc24f5eac2889c25b374c2fe77b46aad08cb154
Phil Shapiro <philshap@gmail.com>
2026-04-07 18:13:44 -0400

    Initial Commit

---
+++ player/playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  playerApp.swift
+//  player
+//
+//  Created by Phil Shapiro on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
